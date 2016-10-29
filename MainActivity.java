633026522ee42040d0c2306b656591b4b923d189
hrker <hyorkim11@gmail.com>
2016-10-29 01:51:10 -0400

    Oct 29 1:51AM

--- MainActivity.java
+++ MainActivity.java
@@ -13,17 +13,19 @@ import android.os.Build;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileFilter;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    private TextView tvDOS, tvDAPI, tvDCPU;
+    private TextView tvDOS, tvDAPI, tvDNumCore, tvChipSet, tvCPUF;
     public String deviceOS = Build.VERSION.RELEASE;
     public int deviceAPI = Build.VERSION.SDK_INT;
-    private TextView textInfo;
 
+    private CalcTemp CT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,14 +47,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void init() {
+        CT = new CalcTemp();
         tvDOS = (TextView) findViewById(R.id.tvDOS);
         tvDAPI = (TextView) findViewById(R.id.tvDAPI);
-        tvDCPU = (TextView) findViewById(R.id.tvDCPU);
+        tvDNumCore = (TextView) findViewById(R.id.tvDNumCores);
+        tvCPUF = (TextView) findViewById(R.id.tvCPUF);
 
-        textInfo = (TextView) findViewById(R.id.tvInfo);
-
+        tvChipSet = (TextView) findViewById(R.id.tvChipSet);
 
     }
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -75,9 +80,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void updateTV() {
-        tvDOS.setText("Device OS: " + deviceOS);
-        tvDAPI.setText("Device API: " + deviceAPI);
-        textInfo.setText(getInfo());
+        tvDOS.setText("OS: " + deviceOS);
+        tvDAPI.setText("API: " + deviceAPI);
+        tvDNumCore.setText("Num Cores: " + getNumCores());
+        tvChipSet.setText(getInfo());
+        CT.printCPUFreq(tvCPUF);
+
     }
 
     private String getInfo() {
@@ -99,5 +107,46 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         return sb.toString();
+    }
+
+
+    private int getNumCores() {
+        if(Build.VERSION.SDK_INT >= 17) {
+            return Runtime.getRuntime().availableProcessors();
+        }
+        else {
+            return getNumCoresOld();
+        }
+    }
+
+    /**
+     * Gets the number of cores available in this device, across all processors.
+     * Requires: Ability to peruse the filesystem at "/sys/devices/system/cpu"
+     * @return The number of cores, or 1 if failed to get result
+     */
+    private int getNumCoresOld() {
+        //Private Class to display only CPU devices in the directory listing
+        class CpuFilter implements FileFilter {
+            @Override
+            public boolean accept(File pathname) {
+                //Check if filename is "cpu", followed by a single digit number
+                if(Pattern.matches("cpu[0-9]+", pathname.getName())) {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        try {
+            //Get directory containing CPU info
+            File dir = new File("/sys/devices/system/cpu/");
+            //Filter to only list the devices we care about
+            File[] files = dir.listFiles(new CpuFilter());
+            //Return the number of cores (virtual CPU devices)
+            return files.length;
+        } catch(Exception e) {
+            //Default to return 1 core
+            return 1;
+        }
     }
 }

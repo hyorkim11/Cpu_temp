15760e9c65f4c20a9ef8860f7454ff04ff74417d
hrker <hyorkim11@gmail.com>
2016-11-02 00:32:56 -0400

    Nov 2 12:32AM

--- ScanPath.java
+++ ScanPath.java
@@ -11,43 +11,60 @@ public class ScanPath extends Activity {
 
 
     private String[] temp_urls = new String[]{
-        "/sys/devices/system/cpu/cpu0/cpufreq/cpu_temp",
+            "/sys/devices/system/cpu/cpu0/cpufreq/cpu_temp",
             "/sys/devices/system/cpu/cpu0/cpufreq/FakeShmoo_cpu_temp",
-            "/sys/class/thermal/thermal_zone0/temp",
-            "/sys/class/i2c-adapter/i2c-4/4-004c/temperature",
+            "/sys/devices/system/cpu/cpufreq/cput_attributes/cur_temp",
+            "/sys/devices/platform/tegra_tmon/temp1_input",
             "/sys/devices/platform/tegra-i2c.3/i2c-4/4-004c/temperature",
             "/sys/devices/platform/omap/omap_temp_sensor.0/temperature",
-            "/sys/devices/platform/tegra_tmon/temp1_input",
-            "/sys/kernel/debug/tegra_thermal/temp_tj",
             "/sys/devices/platform/s5p-tmu/temperature",
-            "/sys/class/thermal/thermal_zone1/temp",
-            "/sys/class/hwmon/hwmon0/device/temp1_input",
-            "/sys/devices/virtual/thermal/thermal_zone1/temp",
             "/sys/devices/platform/s5p-tmu/curr_temp",
             "/sys/devices/virtual/thermal/thermal_zone0/temp",
+            "/sys/devices/virtual/thermal/thermal_zone1/temp",
+            "/sys/devices/virtual/K3G_GYRO-dev/k3g/gyro_get_temp",
+            "/sys/kernel/debug/tegra_thermal/temp_tj",
+            "/sys/class/thermal/thermal_zone0/temp",
+            "/sys/class/thermal/thermal_zone1/temp",
+            "/sys/class/thermal/thermal_zone2/temp",
             "/sys/class/thermal/thermal_zone3/temp",
-            "/sys/class/thermal/thermal_zone4/temp"
+            "/sys/class/thermal/thermal_zone4/temp",
+            "/sys/class/hwmon/hwmon0/device/temp1_input",
+            "/sys/class/i2c-adapter/i2c-4/4-004c/temperature",
     };
+
 
     public int startScan() {
         // takes the urls and scans them
         // returns 0 if none found
+        // default value retrieved in celsius
         String x;
         int temp = 0, temp2 = 0;
         boolean foundAlready = false;
-        for (int i = 0; i < 16; i++) {
+
+        for (int i = 0; i < temp_urls.length; i++) {
+
             try {
                 RandomAccessFile reader = new RandomAccessFile(temp_urls[i], "r");
                 x = reader.readLine();
+
                 if (foundAlready) {
                     // if temp value already found
                     // take new one and average it with existing
                     temp2 = Integer.parseInt(x);
+
+                    if (temp2 > 100) {
+                        temp2 = (temp2 / 1000);
+                    }
+
                     temp = (temp + temp2)/2;
+                    Log.d("EXCEPTION: ", "value " + temp2 + " found at p" + i);
                     temp2 = 0;
                 } else {
                     // if temp value hasn't been found yet, but found one!
                     temp = Integer.parseInt(x);
+                    if (temp > 100) {
+                        temp = (temp/1000);
+                    }
                     foundAlready = true;
                 }
                 reader.close();
@@ -57,6 +74,8 @@ public class ScanPath extends Activity {
         }
         // this converts into Fahrenheit
         temp = (int)(temp*1.8)+32;
+
+        // just return the celsius value
         return temp;
 
     }
